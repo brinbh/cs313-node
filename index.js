5e673db170289c506508124db67368957585b96f
Brittany Hill <brinbh@ldschurch.org>
2018-03-10 10:42:12 -0700

    fixing port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const express = require('express')
 // const path = require('path')
-// const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 //
 // express()
 //   .use(express.static(path.join(__dirname, 'public')))
@@ -126,6 +126,6 @@ function calculateRate(mailType, weight) {
   return price;
 }
 
-app.listen(3000)
+app.listen(PORT)
 
 console.log('listening on port 3000')
