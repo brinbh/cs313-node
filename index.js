bcce5c1d2352029f9e31f805161f3ac5a8c92a8c
Brittany Hill <brinbh@ldschurch.org>
2018-03-30 11:10:26 -0600

    getting on heroku

--- index.js
+++ index.js
@@ -1,38 +1,56 @@
 const express = require('express');
 var router = express.Router();
-const app = express();
+var bodyParser = require('body-parser');
 const pg = require('pg');
 const url = require('url');
 const connectionString = 'postgres://postgres:postgres@localhost:5432/stories';
+const app = express();
+const { Client } = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
+console.log("PORT: " + process.env.PORT);
 console.log("entering index.js");
 
 if (process.env.PORT) {
-  var db_url = url.parse('postgres://xxhxbtldlnxyez:2b9e3d43cb295598843f53e36c72ce158389bc5109a746509dd4e1a6dee32ee0@ec2-54-221-212-15.compute-1.amazonaws.com:5432/d2uc0f94loimtc');
-  // var scheme = db_url.protocol.substr(0, db_url.protocol.length - 1);
-  var user = db_url.auth.substr(0, db_url.auth.indexOf(':'));
-  var pass = db_url.auth.substr(db_url.auth.indexOf(':') + 1, db_url.auth.length);
-  var host = db_url.host.substr(0, db_url.host.indexOf(':'));
-  var port = db_url.host.substr(db_url.host.indexOf(':') + 1, db_url.host.length);
-  var db = db_url.path.substr(db_url.path.indexOf('/') + 1, db_url.path.length);
-
+  console.log("entering heroku client connection on port: " + process.env.PORT);
+  // var db_url = url.parse('postgres://xxhxbtldlnxyez:2b9e3d43cb295598843f53e36c72ce158389bc5109a746509dd4e1a6dee32ee0@ec2-54-221-212-15.compute-1.amazonaws.com:5432/d2uc0f94loimtc');
+  // // var scheme = db_url.protocol.substr(0, db_url.protocol.length - 1);
+  // var user = db_url.auth.substr(0, db_url.auth.indexOf(':'));
+  // var pass = db_url.auth.substr(db_url.auth.indexOf(':') + 1, db_url.auth.length);
+  // var host = db_url.host.substr(0, db_url.host.indexOf(':'));
+  // var port = db_url.host.substr(db_url.host.indexOf(':') + 1, db_url.host.length);
+  // var db = db_url.path.substr(db_url.path.indexOf('/') + 1, db_url.path.length);
+  //
+  // const client = new Client({
+  //   host: host,
+  //   user: user,
+  //   database: db,
+  //   password: pass,
+  //   port: port
+  // });
   const client = new Client({
-    host: host,
-    user: user,
-    database: db,
-    password: pass,
-    port: port
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   });
-
-  console.log('host: ' + client.host + ' user: ' + client.user + ' password: ' + client.password );
 
 }
 
 app.use(express.static(__dirname + '/public'));
+// app.use(bodyParser());
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.post('/login', function (req, res) {
+  username: req.body.username;
+  password:  req.body.password;
+  console.log(username + " " + password);
+})
+
+app.get('/logout', function (req, res) {
+})
 
 // routing to different pages
 
@@ -40,32 +58,40 @@ app.get('/', function (req, res) {
   res.render('index')
 });
 
-app.get('/story/:id', function (req, res) {
-  var story = getStory(req, res);
-  console.log("rendering story: " + story);
-  res.render('story', {story: story});
+app.get('/add', function (req, res) {
+  res.render('add')
 });
 
-// access points
-// app.get('/getStory', function(request, response) {
-//   getStory(request, response);
-// });
+app.post('/addStory', urlencodedParser, function (req, res) {
+  // addStory(req, res);
+  res.render('success', {data: req.body});
+  // res.end(JSON.stringify(req.body))
+});
+
+app.get('/getStory/:id', function (req, res) {
+  getStory(req, res);
+});
+
+app.get('/story/:id', function (req, res) {
+  res.render('story');
+});
 
 app.get('/getAllStories', function(request, response) {
   getAllStories(request, response);
 });
 
+/* PORT Listening */
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
 /*
 GET ALL STORIES
  */
 
 function getAllStories(request, response) {
-
+  console.log("entering getAllStories()");
   // use a helper function to query the DB, and provide a callback for when it's done
   getAllStoriesFromDb( function(error, result) {
 
@@ -82,7 +108,7 @@ function getAllStories(request, response) {
 }
 
 function getAllStoriesFromDb(callback) {
-
+  console.log("entering getAllStoriesFromDb()");
   var client = (this.client || new pg.Client(connectionString));
 
   client.connect(function (err) {
@@ -170,10 +196,62 @@ function getStoryFromDb(id, callback) {
         callback(err, null);
       }
 
-      // console.log("Found result: " + JSON.stringify(result.rows));
+      callback(null, result.rows);
+    });
+  });
+}
 
-      // call whatever function the person that called us wanted, giving it
-      // the results that we have been compiling
+function addStory(request, response) {
+  console.log("entering addStory()");
+  var author = request.body.author;
+  var story = {
+    title: request.body.title,
+    content: request.body.content
+  }
+  debugger
+  // use a helper function to query the DB, and provide a callback for when it's done
+  // addStoryToDb(function(error, result) {
+  //
+  //   // Make sure we got a row with the person, then prepare JSON to send back
+  //   if (error || result == null || result.length != 1) {
+  //     response.status(500).json({success: false, data: error});
+  //   } else {
+  //     response.status(200).json(result[0]);
+  //   }
+  // });
+
+  console.log("response: " + response);
+
+  return response;
+
+}
+
+function addStoryToDb(callback) {
+  console.log("entering addStoryToDb() ");
+
+  var client = (this.client || new pg.Client(connectionString));
+
+  client.connect(function (err) {
+    if (err) {
+      console.log("Error connecting to DB: ")
+      console.log(err);
+      callback(err, null);
+    }
+    // insert into stories (stories_title, stories_content) values ('Story Title', 'This story is awesome');
+    var sql = "INSERT INTO stories (stories_title, stories_content, stories_author) VALUES ()";
+
+    var query = client.query(sql, function (err, result) {
+      // we are now done getting the data from the DB, disconnect the client
+      client.end(function (err) {
+        if (err) throw err;
+      });
+
+      if (err) {
+        console.log("Error in query: ");
+        console.log(err);
+        callback(err, null);
+      }
+
       callback(null, result.rows);
     });
   });
